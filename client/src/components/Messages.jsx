b0869029f79bc1c817f8e91bb7405da15dc23d2d
M A Alim <ma.alim@curix.ai>
2023-03-14 20:14:27 +0600

    introduce pagination

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,20 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { throttle, debounce } from 'lodash';
 import { CHAT_BOT } from '../Constants';
 import Message from './Message';
 
 
 const Messages = ({ socket, roomID }) => {
     const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [more, setMore] = useState(true);
+    const scrollPos = useRef(null);
+    const _start = useRef(null);
 
     useEffect(() => {
-        fetch(`/api/v1/chats/${roomID}`)
-            .then(res => res.json())
-            .then(({ result }) => {
-                result.reverse();
-                setMessages(result);
-            })
-            .catch(() => {});
+        fetchMessages();
     }, []);
+
+    const checkForMsg = (e) => {
+        if(e.target.scrollTop === 0) {
+            fetchMessages(_start.current);
+        }
+    };
 
     useEffect(() => {
         socket && socket.on("receive_message", (_data) => {
@@ -29,8 +34,27 @@ const Messages = ({ socket, roomID }) => {
         msgCont.scrollTop = msgCont.scrollHeight;
     }, [messages]);
 
+    const fetchMessages = (start) => {
+        if(more) {
+            setLoading(true);
+            fetch(`/api/v1/chats/${roomID}?start=${start ? start : ""}`)
+            .then(res => res.json())
+            .then(({ result, lastTime }) => {
+                setLoading(false);
+                if(result.length === 0) setMore(false);
+                result.reverse();
+                setMessages((state) => [...result, ...state]);
+                _start.current = lastTime;
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+        }
+    };
+
     return (
-        <div className="h-83/100 px-4 overflow-y-auto" id="msg_container">
+        <div className="h-83/100 px-4 overflow-y-auto" id="msg_container" onScroll={debounce(checkForMsg, 1000)}>
+            {loading && <div className="w-fit mx-auto my-2">Loading...</div>}
             {messages && messages.map((message, index) => {
                 if(message.sender.username === CHAT_BOT) {
                     return <div key={index} className="py-2 text-xs w-fit m-auto opacity-70">{message.message}</div>
--- routes/ChatRoute.js
+++ routes/ChatRoute.js
@@ -6,14 +6,19 @@ import { getToken } from "../utils/Auth.js";
 const ChatRoute = express.Router();
 
 ChatRoute.get("/:room", async (req, res) => {
+    const startTime = req.query.start !== "" ? req.query.start : Date.now();
+
     const chats = await Chat
-                            .find({ "room": req.params.room })
+                            .find({ "room": req.params.room, "timecreated": { $lt: startTime } })
+                            .limit(10)
                             .populate("sender", { "username": 1 })
                             .sort("-timecreated")
                             .exec();
+                            
+    const lastTime = chats && chats.length > 0 ? chats[chats.length - 1]["timecreated"] : "";
 
     if(chats) {
-        res.status(200).json({ "message": "Rooms found successfully", "result": chats });
+        res.status(200).json({ "message": "Rooms found successfully", "result": chats, "lastTime": lastTime });
     } else {
         res.status(500).json({ "message": "Something went wrong" });
     }
